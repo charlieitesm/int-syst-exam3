10929c6349109faee3372162dc568f4629929fe1
C Hz <47310270+charlieitesm@users.noreply.github.com>
2019-05-12 23:38:11 -0500

    Exercise 6 - Add calculation of Gaussian Naive Bayes Theorem

--- src/exercise6_dataframes.py
+++ src/exercise6_dataframes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import math
 
 
-def calculate_probability(mean: float, stdev: float, x: float):
+def calculate_normal_probability_distribution(mean: float, stdev: float, x: float):
     pi = 3.14159
     euler = 2.71828
 
@@ -16,7 +16,7 @@ def calculate_probability(mean: float, stdev: float, x: float):
     return probability
 
 
-def calculate_posterior(features_posteriors: list, label_posterior: float):
+def calculate_gaussian_naive_bayes(features_posteriors: list, label_posterior: float):
     posterior = 1
 
     # Multiply all of the features posteriors to in order to calculate Naive Bayes
@@ -29,22 +29,22 @@ def calculate_posterior(features_posteriors: list, label_posterior: float):
 
 
 male_fp = [
-    calculate_probability(5.855, 0.187171935, 6),
-    calculate_probability(176.25, 11.08677891, 130),
-    calculate_probability(11.25, 0.957427108, 8)
+    calculate_normal_probability_distribution(5.855, 0.187171935, 6),
+    calculate_normal_probability_distribution(176.25, 11.08677891, 130),
+    calculate_normal_probability_distribution(11.25, 0.957427108, 8)
 ]
 
-posterior_male = calculate_posterior(male_fp, 0.5)
+posterior_male = calculate_gaussian_naive_bayes(male_fp, 0.5)
 
 female_fp = [
-    calculate_probability(5.4175, 0.311809237, 6),
-    calculate_probability(132.5, 23.62907813, 130),
-    calculate_probability(7.5, 1.290994449, 8)
+    calculate_normal_probability_distribution(5.4175, 0.311809237, 6),
+    calculate_normal_probability_distribution(132.5, 23.62907813, 130),
+    calculate_normal_probability_distribution(7.5, 1.290994449, 8)
 ]
 
-posterior_female = calculate_posterior(female_fp, 0.5)
+posterior_female = calculate_gaussian_naive_bayes(female_fp, 0.5)
 
-# making data frame from csv file
+# Read the training dataset
 data = pd.read_csv("../Jupyter/wine.csv", skipinitialspace=True, skip_blank_lines=True)
 
 data.sort_values("cultivar", inplace=True)
@@ -63,5 +63,76 @@ label_3_means = label_3.mean(axis=0)
 label_1_stdev = label_1.std(axis=0, ddof=0)
 label_2_stdev = label_2.std(axis=0, ddof=0)
 label_3_stdev = label_3.std(axis=0, ddof=0)
+
+# Read the test dataset
+test_data = pd.read_csv("../Jupyter/wineTestModel.csv", skipinitialspace=True, skip_blank_lines=True)
+
+# Drop the cultivar label as this is the one we will calculate
+test_data.drop("cultivar", axis=1, inplace=True)
+
+# Prepare the variables that we'll use for the calculation and drop the cultivar label
+feature_list = list(test_data.columns.values)
+
+# The posteriors, that is P(label_1), is equal to the number of cultivar==1, 2 or 3 divided by the total num of records
+posterior_1 = len(label_1) / len(data)
+posterior_2 = len(label_2) / len(data)
+posterior_3 = len(label_3) / len(data)
+
+# We'll add all of the information in symmetric lists in order to ease the calculation loop structure
+cultivar_label_posteriors = [posterior_1, posterior_2, posterior_3]
+means = [label_1_means, label_2_means, label_3_means]
+stdevs = [label_1_stdev, label_2_stdev, label_3_stdev]
+
+# Begin the calculation
+for row_index, row in test_data.iterrows():
+    print("\n\n***************************")
+    print(f"Calculating label for row {row_index + 1}...")
+    print("***************************\n")
+
+    cultivar_label_bayes_probabilities = []
+
+    # Check each of the labels
+    for label_idx in range(3):
+
+        print(f"\n-------Calculating P(cultivar == {label_idx + 1})...\n")
+
+        features_gauss = []
+
+        for feature in feature_list:
+            feature_x = row[feature]
+            feature_mean = means[label_idx][feature]
+            feature_stdev = stdevs[label_idx][feature]
+
+            feature_gauss = calculate_normal_probability_distribution(mean=feature_mean,
+                                                                      stdev=feature_stdev,
+                                                                      x=feature_x)
+
+            features_gauss.append(feature_gauss)
+
+            print(f"-------Feature: {feature} -------")
+            print(f"X = {feature_x}")
+            print(f"Feature mean = {feature_mean}")
+            print(f"Feature Std Dev = {feature_stdev}")
+            print(f"P({feature}| Cultivar == {label_idx +1}) = {feature_gauss}")
+
+        # Calculate the posterior of the label given all of the features P(
+        cultivar_posterior = cultivar_label_posteriors[label_idx]
+
+        label_probability = calculate_gaussian_naive_bayes(features_gauss, cultivar_posterior)
+
+        cultivar_label_bayes_probabilities.append(label_probability)
+
+        print(f"\n >>>>>>>> P(cultivar == {label_idx + 1} | {','.join(feature_list)}): {label_probability}")
+
+    print(f"\n ================ RESULTS FOR ROW {row_index + 1} =================")
+    print("Given the following Bayes posteriors for each label:")
+
+    for result_idx, result in enumerate(cultivar_label_bayes_probabilities):
+        print(f"P(cultivar == {result_idx + 1}): {result}")
+
+    # To get the final value, we just get the index in the label probabilities and offset the value by 1
+    final_cultivar = cultivar_label_bayes_probabilities.index(max(cultivar_label_bayes_probabilities)) + 1
+    print(f"ROW {row_index + 1} is probably Cultivar {final_cultivar}")
+
 
 print("Hello")
