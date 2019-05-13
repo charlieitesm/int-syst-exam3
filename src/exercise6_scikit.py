d9e2bb30b87108ad00e1103ea838873e78a3e3fd
C Hz <47310270+charlieitesm@users.noreply.github.com>
2019-05-13 13:43:24 -0500

    Exercise 6 - Add a test_split using sklearn example

--- src/exercise6_scikit.py
+++ src/exercise6_scikit.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 gnb = GaussianNB()
 
@@ -19,5 +21,25 @@ test_data.insert(loc=0, column='cultivar', value=prediction)
 
 print(test_data.to_string())
 
+# Using test splitting
+
+gnb = GaussianNB()
+
+data = pd.read_csv("../Jupyter/wine.csv", skipinitialspace=True, skip_blank_lines=True)
+training_data = pd.read_csv("../Jupyter/wine.csv", skipinitialspace=True, skip_blank_lines=True)
+training_labels = training_data["cultivar"]
+
+training_data.drop("cultivar", inplace=True, axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    training_data, training_labels, random_state=1988
+)
+
+gnb.fit(X_train, y_train)
+
+prediction = gnb.predict(X_test)
+
+print(confusion_matrix(y_test, prediction))
+print(f"Accuracy: {accuracy_score(y_test, prediction)}")
 
 
